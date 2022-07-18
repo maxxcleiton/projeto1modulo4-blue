317d017473b236ca3978398d807beec52dcba099
maxxcleiton <maxcleiton@live.com>
2022-07-18 00:08:51 -0300

    update - names changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MeuTodoList from './components/MeuTodoList';
+import { ToDoList } from './components/ToDoList';
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer';
 
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <MeuTodoList />
-    <Footer />
+    <ToDoList />
+    {/* <Footer /> */}
   </React.StrictMode>
 );
